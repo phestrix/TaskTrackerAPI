15c477b3f1c7739076c148f55f742bd3ce157de2
matri <matri@NITRO5>
2023-10-03 20:53:16 +0700

    started implementing post request with creation of project

--- src/main/java/ru/phestrix/tasktrackerapi/api/controllers/ProjectController.java
+++ src/main/java/ru/phestrix/tasktrackerapi/api/controllers/ProjectController.java
@@ -4,8 +4,13 @@ import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.phestrix.tasktrackerapi.api.dto.ProjectDTO;
 import ru.phestrix.tasktrackerapi.api.factories.ProjectDtoFactory;
+import ru.phestrix.tasktrackerapi.storage.entities.ProjectEntity;
+import ru.phestrix.tasktrackerapi.storage.repositories.ProjectRepository;
 
 @RestController
 @Transactional
@@ -14,6 +19,13 @@ import ru.phestrix.tasktrackerapi.api.factories.ProjectDtoFactory;
 public class ProjectController {
 
     ProjectDtoFactory projectDtoFactory;
+    ProjectRepository projectRepository;
+    public static final String CREATE_PROJECT = "api/projects";
+
+    @PostMapping
+    public ProjectDTO createProject(@RequestParam String name) {
+        ProjectEntity entity =
+    }
 
 
 }
