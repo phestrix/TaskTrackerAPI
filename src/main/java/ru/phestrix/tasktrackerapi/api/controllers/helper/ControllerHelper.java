4e90a240b219e9c56a88f4e31fc47d211c13c2df
matri <matri@NITRO5>
2023-10-04 21:25:29 +0700

    added method to get task state or throw exception

--- src/main/java/ru/phestrix/tasktrackerapi/api/controllers/helper/ControllerHelper.java
+++ src/main/java/ru/phestrix/tasktrackerapi/api/controllers/helper/ControllerHelper.java
@@ -7,7 +7,9 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
 import ru.phestrix.tasktrackerapi.api.exceptions.NotFoundException;
 import ru.phestrix.tasktrackerapi.storage.entities.ProjectEntity;
+import ru.phestrix.tasktrackerapi.storage.entities.TaskStateEntity;
 import ru.phestrix.tasktrackerapi.storage.repositories.ProjectRepository;
+import ru.phestrix.tasktrackerapi.storage.repositories.TaskStateRepository;
 
 @RequiredArgsConstructor
 @Transactional
@@ -15,9 +17,19 @@ import ru.phestrix.tasktrackerapi.storage.repositories.ProjectRepository;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ControllerHelper {
     ProjectRepository projectRepository;
+    TaskStateRepository taskStateRepository;
+
     public ProjectEntity getProjectEntityOrThrowException(Long projectId) {
         return projectRepository
                 .findById(projectId)
                 .orElseThrow(() -> new NotFoundException("Project with id: " + projectId + "doesnt exist"));
+    }
+
+    public TaskStateEntity getTaskStateEntityOrThrowException(Long taskStateId) {
+        return taskStateRepository
+                .findById(taskStateId)
+                .orElseThrow(() ->
+                        new NotFoundException("Task State with id " + taskStateId + "doesnt exist")
+                );
     }
 }
