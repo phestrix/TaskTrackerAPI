4c31223ab4af1cb5db66ebefc9e2e677f4daf61f
matri <matri@NITRO5>
2023-10-04 21:22:47 +0700

    added method to search task state

--- src/main/java/ru/phestrix/tasktrackerapi/storage/repositories/TaskStateRepository.java
+++ src/main/java/ru/phestrix/tasktrackerapi/storage/repositories/TaskStateRepository.java
@@ -3,5 +3,8 @@ package ru.phestrix.tasktrackerapi.storage.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.phestrix.tasktrackerapi.storage.entities.TaskStateEntity;
 
+import java.util.Optional;
+
 public interface TaskStateRepository extends JpaRepository<TaskStateEntity, Long> {
+    Optional<TaskStateEntity> findTaskStateEntityByProjectIdAndNameContainsIgnoreCase(Long projectId, String taskStateName);
 }
