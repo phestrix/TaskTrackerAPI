1d37fa5ec2f7936d928a24458448864d7d6005fc
matri <matri@NITRO5>
2023-10-04 20:58:32 +0700

    updated to new task state

--- src/main/java/ru/phestrix/tasktrackerapi/api/factories/TaskStateDtoFactory.java
+++ src/main/java/ru/phestrix/tasktrackerapi/api/factories/TaskStateDtoFactory.java
@@ -1,17 +1,33 @@
 package ru.phestrix.tasktrackerapi.api.factories;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
 import ru.phestrix.tasktrackerapi.api.dto.TaskStateDTO;
 import ru.phestrix.tasktrackerapi.storage.entities.TaskStateEntity;
 
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Component
 public class TaskStateDtoFactory {
+    TaskDtoFactory taskDtoFactory;
     public TaskStateDTO makeTaskStateDTO(TaskStateEntity entity) {
         return TaskStateDTO.builder()
                 .id(entity.getId())
                 .name(entity.getName())
                 .createdAt(entity.getCreatedAt())
-                .ordinal(entity.getOrdinal())
+                .leftTaskState(entity.getLeftTaskState().map(TaskStateEntity::getId).orElse(null))
+                .rightTaskState(entity.getRightTaskState().map(TaskStateEntity::getId).orElse(null))
+                .tasks(
+                        entity
+                                .getTasks()
+                                .stream()
+                                .map(taskDtoFactory::makeTaskDTO)
+                                .collect(Collectors.toList())
+                )
                 .build();
 
     }
