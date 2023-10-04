02ba684d13126b6dc3c66eef875af373e9281e21
matri <matri@NITRO5>
2023-10-04 17:22:35 +0700

    added method to delete project

--- src/main/java/ru/phestrix/tasktrackerapi/api/controllers/ProjectController.java
+++ src/main/java/ru/phestrix/tasktrackerapi/api/controllers/ProjectController.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
+import ru.phestrix.tasktrackerapi.api.dto.AnswerDTO;
 import ru.phestrix.tasktrackerapi.api.dto.ProjectDTO;
 import ru.phestrix.tasktrackerapi.api.exceptions.NotFoundException;
 import ru.phestrix.tasktrackerapi.api.factories.ProjectDtoFactory;
@@ -26,9 +27,12 @@ public class ProjectController {
 
     ProjectDtoFactory projectDtoFactory;
     ProjectRepository projectRepository;
+    private static final boolean SUCCESSFUL_DELETION = true;
+    private static final boolean DELETION_FAILURE = false;
     public static final String CREATE_PROJECT = "api/projects";
     public static final String FETCH_PROJECTS = "/api/projects";
     public static final String EDIT_PROJECT = "api/projects/{project_id}";
+    public static final String DELETE_PROJECT = "api/projects/{project_id}";
 
     @GetMapping(FETCH_PROJECTS)
     public List<ProjectDTO> fetchProjects(
@@ -36,6 +40,7 @@ public class ProjectController {
                     value = "prefix_name",
                     required = false)
             Optional<String> optionalPrefixName) {
+
         optionalPrefixName = optionalPrefixName.filter(prefixName -> !prefixName.trim().isEmpty());
 
         Stream<ProjectEntity> projectStream = optionalPrefixName
@@ -81,5 +86,14 @@ public class ProjectController {
         project.setName(name);
         project = projectRepository.saveAndFlush(project);
         return projectDtoFactory.makeProjectDTO(project);
+    }
+
+    @DeleteMapping(DELETE_PROJECT)
+    public AnswerDTO deleteProject(@PathVariable("project_id") Long projectId) {
+        projectRepository
+                .findById(projectId)
+                .orElseThrow(() -> new NotFoundException("Project with id: " + projectId + "doesnt exist"));
+        projectRepository.deleteById(projectId);
+        return AnswerDTO.makeDefault(SUCCESSFUL_DELETION);
     }
 }
