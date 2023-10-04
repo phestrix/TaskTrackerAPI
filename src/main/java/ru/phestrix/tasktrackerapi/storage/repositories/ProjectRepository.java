78f05327153445ac372557c83ae54803a6791895
matri <matri@NITRO5>
2023-10-04 14:54:45 +0700

    added methods for streams

--- src/main/java/ru/phestrix/tasktrackerapi/storage/repositories/ProjectRepository.java
+++ src/main/java/ru/phestrix/tasktrackerapi/storage/repositories/ProjectRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.phestrix.tasktrackerapi.storage.entities.ProjectEntity;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
     Optional<ProjectEntity> findProjectEntityByName(String name);
+    Stream<ProjectEntity> StreamAllBy();
+    Stream<ProjectEntity> streamAllByNameStartsWithIgnoreCase(String prefixName);
 }
