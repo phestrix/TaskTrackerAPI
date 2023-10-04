91d356394d27f781273e7cb832ed1b2430f79f48
matri <matri@NITRO5>
2023-10-04 21:17:39 +0700

    added method to create task state

--- src/main/java/ru/phestrix/tasktrackerapi/api/controllers/TaskStateController.java
+++ src/main/java/ru/phestrix/tasktrackerapi/api/controllers/TaskStateController.java
@@ -4,17 +4,18 @@ import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.phestrix.tasktrackerapi.api.controllers.helper.ControllerHelper;
 import ru.phestrix.tasktrackerapi.api.dto.TaskDTO;
 import ru.phestrix.tasktrackerapi.api.dto.TaskStateDTO;
+import ru.phestrix.tasktrackerapi.api.exceptions.BadRequestException;
 import ru.phestrix.tasktrackerapi.api.factories.TaskStateDtoFactory;
 import ru.phestrix.tasktrackerapi.storage.entities.ProjectEntity;
+import ru.phestrix.tasktrackerapi.storage.entities.TaskStateEntity;
 import ru.phestrix.tasktrackerapi.storage.repositories.TaskStateRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -38,5 +39,52 @@ public class TaskStateController {
                 .stream()
                 .map(taskStateDtoFactory::makeTaskStateDTO)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping(CREATE_TASKS_STATE)
+    public TaskStateDTO createTaskState(
+            @PathVariable(name = "project_id") Long projectId,
+            @RequestParam(name = "task_state_name") String taskStateName
+    ){
+        if (taskStateName.trim().isEmpty()) {
+            throw new BadRequestException("Task state name can't be empty.");
+        }
+
+        ProjectEntity project = controllerHelper.getProjectEntityOrThrowException(projectId);
+
+        Optional<TaskStateEntity> optionalAnotherTaskState = Optional.empty();
+
+        for (TaskStateEntity taskState: project.getTaskStates()) {
+
+            if (taskState.getName().equalsIgnoreCase(taskStateName)) {
+                throw new BadRequestException(String.format("Task state \"%s\" already exists.", taskStateName));
+            }
+
+            if (taskState.getRightTaskState().isEmpty()) {
+                optionalAnotherTaskState = Optional.of(taskState);
+                break;
+            }
+        }
+
+        TaskStateEntity taskState = taskStateRepository.saveAndFlush(
+                TaskStateEntity.builder()
+                        .name(taskStateName)
+                        .project(project)
+                        .build()
+        );
+
+        optionalAnotherTaskState
+                .ifPresent(anotherTaskState -> {
+
+                    taskState.setLeftTaskState(anotherTaskState);
+
+                    anotherTaskState.setRightTaskState(taskState);
+
+                    taskStateRepository.saveAndFlush(anotherTaskState);
+                });
+
+        final TaskStateEntity savedTaskState = taskStateRepository.saveAndFlush(taskState);
+
+        return taskStateDtoFactory.makeTaskStateDTO(savedTaskState);
     }
 }
