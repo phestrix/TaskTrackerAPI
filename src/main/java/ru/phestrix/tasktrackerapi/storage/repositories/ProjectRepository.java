005160bf4ff89217715f6b2ab816437ca4a4a584
matri <matri@NITRO5>
2023-10-04 14:06:38 +0700

    added method to find by name

--- src/main/java/ru/phestrix/tasktrackerapi/storage/repositories/ProjectRepository.java
+++ src/main/java/ru/phestrix/tasktrackerapi/storage/repositories/ProjectRepository.java
@@ -3,5 +3,8 @@ package ru.phestrix.tasktrackerapi.storage.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.phestrix.tasktrackerapi.storage.entities.ProjectEntity;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+    Optional<ProjectEntity> findProjectEntityByName(String name);
 }
